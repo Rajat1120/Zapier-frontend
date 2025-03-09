2f93a8cb29c4e7391b73e61fc8a2ac3ddeb369e6
Rajat1120 <rajat812678@gmail.com>
2025-03-09 09:57:12 +0530

    initial commit

---
+++ components/Hero.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { useRouter } from "next/navigation";
+// import { Feature } from "./Feature";
+import { PrimaryButton } from "./buttons/PrimaryButton";
+import { SecondaryButton } from "./buttons/SecondaryButtons";
+import { Feature } from "./Feature";
+
+export const Hero = () => {
+  const router = useRouter();
+  return (
+    <div>
+      <div className="flex justify-center">
+        <div className="text-5xl-custom font-bold text-center pt-8 max-w-xl">
+          Automate as fast as you can type
+        </div>
+      </div>
+      <div className="flex justify-center pt-2">
+        <div className="text-xl font-normal  text-center pt-8 max-w-3xl">
+          AI gives you automation superpowers, and Zapier puts them to work.
+          Pairing AI and Zapier helps you turn ideas into workflows and bots
+          that work for you.
+        </div>
+      </div>
+
+      <div className="flex justify-center pt-4">
+        <div className="flex">
+          <PrimaryButton
+            onClick={() => {
+              router.push("/signup");
+            }}
+            size="big"
+          >
+            Get Started free
+          </PrimaryButton>
+          <div className="pl-4">
+            <SecondaryButton onClick={() => {}} size="big">
+              Contact Sales
+            </SecondaryButton>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex  justify-center space-x-1 pt-4">
+        <Feature title={"Free Forever"} subtitle={"for core features"} />
+        <Feature title={"More apps"} subtitle={"than any other platforms"} />
+        <Feature title={"Cutting Edge"} subtitle={"AI Features"} />
+      </div>
+    </div>
+  );
+};
