a533607017a3d844ba4a07d64f2ad0462b6519ed
Rajat1120 <rajat812678@gmail.com>
2025-06-13 16:09:49 +0530

    Feat: show tooltip with text Add step on hover

--- utils/CustomEdge.tsx
+++ utils/CustomEdge.tsx
@@ -35,21 +35,68 @@ const CustomEdge = ({
             transform: "translate(-50%, -50%)",
             top: labelY,
             left: labelX,
-            fontSize: 12,
-            padding: "0px 0px",
-            backgroundColor: "#fdf7f2",
-            pointerEvents: "all",
-            cursor: "pointer",
             zIndex: 1000,
-          }}
-          onClick={() => {
-            const event = new CustomEvent("add-node", {
-              detail: { edgeId: id },
-            });
-            window.dispatchEvent(event);
+            pointerEvents: "all",
           }}
         >
-          +
+          <div
+            style={{
+              position: "relative",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: 16,
+              height: 16,
+              borderRadius: "50%",
+              backgroundColor: "#fdf7f2",
+              fontSize: 10,
+              fontWeight: "bold",
+              cursor: "pointer",
+              transition: "transform 0.2s ease",
+            }}
+            onClick={() => {
+              const event = new CustomEvent("add-node", {
+                detail: { edgeId: id },
+              });
+              window.dispatchEvent(event);
+            }}
+            onMouseEnter={(e) => {
+              e.currentTarget.style.transform = "scale(1.2)";
+              const tooltip = e.currentTarget.querySelector(
+                ".tooltip"
+              ) as HTMLElement | null;
+              if (tooltip) tooltip.style.opacity = "1";
+            }}
+            onMouseLeave={(e) => {
+              e.currentTarget.style.transform = "scale(1)";
+              const tooltip = e.currentTarget.querySelector(
+                ".tooltip"
+              ) as HTMLElement | null;
+              if (tooltip) tooltip.style.opacity = "0";
+            }}
+          >
+            +
+            <div
+              className="tooltip"
+              style={{
+                position: "absolute",
+                top: "25%",
+                left: "230%",
+                transform: "translateX(-50%)",
+                backgroundColor: "black",
+                color: "white",
+                padding: "2px 4px",
+                borderRadius: "2px",
+                fontSize: "6px",
+                whiteSpace: "nowrap",
+                pointerEvents: "none",
+                opacity: 0,
+                transition: "opacity 0.2s ease",
+              }}
+            >
+              Add step
+            </div>
+          </div>
         </div>
       </EdgeLabelRenderer>
     </>
