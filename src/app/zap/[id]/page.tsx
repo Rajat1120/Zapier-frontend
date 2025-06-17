201cdbef1ed4c94cb98eaae8f77a71fa2de30cf5
Rajat1120 <rajat812678@gmail.com>
2025-06-17 10:19:57 +0530

    update [id]/page.tsx code to match the UI with create zap page

--- src/app/zap/[id]/page.tsx
+++ src/app/zap/[id]/page.tsx
@@ -1,18 +1,28 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useParams } from "next/navigation";
 import ActionsList from "../../../../components/ActionList";
 
 export default function ZapPage() {
   const params = useParams();
-
+  const router = useRouter();
   return (
-    <div className="h-full w-full">
-      <h1 className="text-2xl font-bold">Zapier</h1>
-
-      <ActionsList
-        id={Array.isArray(params.id) ? params.id[0] : params.id}
-      ></ActionsList>
+    <div className="h-full w-full flex">
+      <div className="h-full bg-[#413735] flex justify-center py-4 w-10">
+        <img
+          onClick={() => router.push("/dashboard")}
+          className="cursor-pointer w-8 h-8 "
+          src="https://img.icons8.com/?size=100&id=RYh4BCDIOen9&format=png&color=ffffff"
+          alt=""
+        />
+      </div>
+      <div className="h-full w-full">
+        <ActionsList
+          id={Array.isArray(params.id) ? params.id[0] : params.id}
+        ></ActionsList>
+        ;
+      </div>
     </div>
   );
 }
