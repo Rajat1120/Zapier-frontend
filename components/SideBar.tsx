c52cdfe59e69ed3cd621dd35662abcaa529cfa42
Rajat1120 <rajat812678@gmail.com>
2025-07-05 16:29:00 +0530

    add styles to buttons

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -18,22 +18,30 @@ export default function Sidebar() {
     setShowZapModal(false);
   }, [setShowZapModal]);
   return (
-    <div className="fixed top-16 right-4 w-[28%] h-[80%] flex  flex-col border-2 border-[#695be8] bg-[#fdf7f2] rounded-md">
+    <div className="fixed top-16 right-4 w-[28%] h-[80%] flex  flex-col border-2 border-[#695be8] bg-white rounded-md">
       <div className="p-3 rounded-md justify-between  bg-[#f0eefb] flex">
-        <div>
-          img
-          <span>some text</span>
+        <div className="flex items-center space-x-2">
+          <div className="bg-white h-8 w-8 border border-[#d7d3c9] rounded-xs flex items-center justify-center ">
+            <Image
+              className="max-w-6 max-h-6"
+              width={20}
+              height={20}
+              src={image}
+              alt={name}
+            ></Image>
+          </div>
+          <span>Select the Event</span>
         </div>
         <div>
           <span>make it big</span>
-          <span>close it</span>
+          <span>X</span>
         </div>
       </div>
       <div className="flex-grow border-b-[#d7d3c9] border overflow-y-auto">
-        <div className="p-3">setup</div>
+        <div className="p-3">Setup</div>
         <div className=" border-t border-b p-3 h-full border-[#d7d3c9]">
           <div className="flex m-2 flex-col">
-            <span className="mb-1 font-medium">App</span>
+            <span className="mb-1 font-medium text-sm">App</span>
             <div className="w-full rounded-md items-center  flex justify-between p-2 border border-[#d7d3c9]">
               <div className="border flex items-center space-x-2 border-[#d7d3c9] rounded-sm py-1 px-2">
                 <Image
@@ -47,14 +55,14 @@ export default function Sidebar() {
               </div>
               <button
                 onClick={() => setShowZapModal(true)}
-                className="border border-[#d7d3c9] p-1 rounded-sm "
+                className="border px-2 border-[#d7d3c9] text-sm cursor-pointer font-bold text-white bg-[#695be8] p-1 rounded-sm "
               >
                 Change
               </button>
             </div>
           </div>
           <div className="flex m-2 flex-col">
-            <span className="mb-1">Action Event</span>
+            <span className="mb-1 font-medium text-sm">Action Event</span>
             <div className="w-full  flex justify-between p-2 border border-[#d7d3c9] rounded-md">
               <div>
                 <span className="text-sm">Choose an event</span>
@@ -63,12 +71,14 @@ export default function Sidebar() {
             </div>
           </div>
           <div className="flex m-2 flex-col">
-            <span className="mb-1">Account</span>
+            <span className="mb-1 font-medium text-sm">Account</span>
             <div className="w-full  flex justify-between p-2 border border-[#d7d3c9] rounded-md">
               <div>
                 <span className="text-sm">Select An account</span>
               </div>
-              <button>Select</button>
+              <button className="border px-2 border-[#d7d3c9] text-sm cursor-pointer font-bold text-white bg-[#695be8] p-1 rounded-sm ">
+                Select
+              </button>
             </div>
           </div>
           <div className="p-3">
