b54a47e5261f5201056ebb0c31994a4a2509c4a6
Rajat1120 <rajat812678@gmail.com>
2025-08-26 15:47:45 +0530

    Fix: ensure isWordIncluded returns truen in case of email and gmail

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -82,9 +82,10 @@ export const addTrailingPlusNode = (
 };
 
 export function isWordIncluded(str1: string, str2: string) {
+
   const s1 = str1.toLowerCase().replace(/\s+/g, "");
   const s2 = str2.toLowerCase().replace(/\s+/g, "");
-
+  if(s1 === "email" && s2 === "gmail") return true;
   return s2.includes(s1);
 }
 
